143bbf5b6fc12dd95b278b26de65ed842854af65
aditya01933 <adityatiwari01933@gmail.com>
2016-06-01 19:19:25 +0530

    background sidekiq

--- app/workers	/operation_worker.rb
+++ app/workers	/operation_worker.rb
@@ -1,10 +1,34 @@
 class OperationWorker
 	include Sidekiq::Worker
+	sidekiq_options :retry => false
 
-	  def perform(file_path, file_name)
-	  	@operations = Operation.new(file_path: file_path, file_name: file_name)	  	
-	    @operations.custom_save
+	  def perform(file_path, file_name)	  
+
+	    spreadsheet = Roo::CSV.new(file_path)
+	    header = spreadsheet.row(1)   
+		  (2..spreadsheet.last_row).map do |i|
+		    row = Hash[[header, spreadsheet.row(i)].transpose] 	  	 
+		  	operation = add_company(row)
+		  	if operation.valid?
+		  		operation.save!
+		  	else
+		  		operation.full_messages
+		  	end	
+		  end
+
+	  end
+	  
+	  private
+
+	  def add_company(row)
+	  	operation = Operation.new
+	  	company = Company.find_by(name: row["company"])
+	  	operation.attributes = row.except("company", nil)  
+	  	if company.nil?
+	  	  operation
+	  	else
+	  	  company.operations.build(row.except("company", nil).merge(company_id: company.id))
+	  	end
 	  end
 
-	
 end
