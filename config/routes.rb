e2661ebb3c00473cb66ec01cbdd92df241431cae
aditya01933 <adityatiwari01933@gmail.com>
2016-06-02 01:18:53 +0530

    root path added

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
 
   get 'operation_csv' => 'operations#operation_csv'
 
-
+  root :to => "companies#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
