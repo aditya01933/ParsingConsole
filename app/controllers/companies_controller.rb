3c35843e089fdadeadb57317d1473272abfcf609
aditya01933 <adityatiwari01933@gmail.com>
2016-06-02 11:41:54 +0530

    filter feature

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -2,9 +2,13 @@ class CompaniesController < ApplicationController
 	
 
 	def index
-		 @operations = Operation.new	 
+		@operations = Operation.new	 
 		@companies = Company.all.page(params[:page]).per(1)
+		if params[:filter].present?
+			@filter = params[:filter]
+		end
 	end
+
 
 	
 
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   resources :job_statuses
 
   get 'operation_csv' => 'operations#operation_csv'
-
+ 
   root :to => "companies#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
